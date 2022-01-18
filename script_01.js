197d1926b20666804cfadf3b35d25d2207f430aa
Clapfix <ronny.perner@kabelmail.de>
2022-01-18 08:46:57 +0100

    Entscheidungsstrukturen

--- script_01.js
+++ script_01.js
@@ -2,3 +2,21 @@
 
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assigment
+const ageJohn = 30;
+const ageMark = 25;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
